58f3f1e433bf227c2a9c5ff6eb12a774f82cc1b0
trainningjava <48299029+trainningjava@users.noreply.github.com>
2021-02-01 16:50:47 -0300

    desafio1

--- codepix/domain/model/user_test.go
+++ codepix/domain/model/user_test.go
@@ -1,18 +1,18 @@
 package model
 
 import (
-	"testing"
-
-	"github.com/asaskevich/govalidator"
 	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+	"testing"
 )
 
 func TestNewUser(t *testing.T) {
 	name := "User001"
 	email := "user001@email.com"
-	bank, err := model. .NewBank(code, name)
+	user, err := NewUser(name, email)
 
-	accountNumber := "abcnumber"
-	ownerName := "Wesley"
-	account, err := model.NewAccount(bank, accountNumber, ownerName)
+	require.Nil(t, err)
+	require.NotNil(t, uuid.FromStringOrNil(user.ID))
+	require.Equal(t, user.Name, name)
+	require.Equal(t, user.Email, email)
 }
